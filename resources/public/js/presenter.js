1cde930930596bca9d1f222a260c9def7af5a617
Martina <martinagehwolf@gmail.com>
2014-10-07 19:12:17 +0100

    add feedback-board.html and main.css files

--- resources/public/js/presenter.js
+++ resources/public/js/presenter.js
@@ -2,6 +2,7 @@ var WEB_SOCKET_URL = 'ws://example.com:8080/api/presenter';
 
 $(function () {
     var webSocket = new WebSocket(WEB_SOCKET_URL);
+    console.log("starting the connection process");
 
     webSocket.onopen = function () {
         console.log("Connected successfully!");
@@ -14,4 +15,8 @@ $(function () {
     webSocket.onmessage = function (event) {
         console.log("Message received: " + event.data);
     };
+
+
+    var c = $("#canvas")[0];
+    var ctx = c.getContext("2d");
 });
